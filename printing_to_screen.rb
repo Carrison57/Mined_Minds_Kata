a2865af7350137ee2f6ad1ea5fddd48d23f2a27f
Carrison57 <calebarrison@gmail.com>
2016-08-02 21:43:26 -0400

    still not quite there

--- printing_to_screen.rb
+++ printing_to_screen.rb
@@ -1,7 +1,23 @@
 number = 1
 100.times do
 	puts number
-	number = number + 1 	
+	number = number + 1
+
+
+if number % 3 == 0
+	puts "Mined"
+end	
+
+if number % 5 == 0
+	puts "Minds"
 end
+
+
+
+
+
+
+end
+
 
 
