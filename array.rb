49a5296e73e5c7ad85ecb174c4992c001a0f4c65
Carrison57 <calebarrison@gmail.com>
2016-08-10 12:29:33 -0400

    added conditionals to array

--- array.rb
+++ array.rb
@@ -6,23 +6,20 @@ def create_mined_minds_array
 
 	for number in 1..100 do
 		array.push(number)
+		
 		if number % 3 == 0 && number % 5 == 0
 			array.push("Mined Minds")
+		elsif number % 3 == 0
+			array.push("Mined")
+		elsif number % 5 == 0
+			array.push("Minds")
 		end
+	
 	end
 
 	print array
 
-	
-		#elsif number % 3 == 0
-			#puts "Mined"
-		#elsif number % 5 == 0
-			#puts "Minds"
-		#else 
-			#puts number
-		#end
 
-	number += 1
 
 	
 end
