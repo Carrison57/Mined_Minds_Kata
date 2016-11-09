cc27c06ea3d47fc7cf5a1d425ba782266f5a25cf
Carrison57 <calebarrison@gmail.com>
2016-11-09 14:11:02 -0500

    Updated README file.

--- array.rb
+++ array.rb
@@ -1,7 +1,5 @@
 def create_mined_minds_array()
 
-	#number = 1
-
 	array = []
 
 	for number in 1..100 do
@@ -19,8 +17,6 @@ def create_mined_minds_array()
 	end
 
 	return array
-
-	#print array
 
 end
 
